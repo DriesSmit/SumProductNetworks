3ef2805e0d583c397ba1b04a43fc3b663c9db837
DriesSmit <dries.epos@gmail.com>
2018-03-07 17:13:11 +0200

    Inference is now working.

---
+++ SumProductNetwork/Main.py
@@ -0,0 +1,33 @@
+from SumProductNetwork import SPN
+from SumProductNetwork import opp_sum, opp_multi, variable
+
+spn = SPN()
+
+network = []
+
+# Initialize variables
+n_x1 = variable()
+x1 = variable()
+n_x2 = variable()
+x2 = variable()
+
+# The plus operators
+p1 = opp_sum(name = "p1",inherit=[n_x1,x1],probs=[0.2, 0.8])
+p2 = opp_sum(name = "p2",inherit=[n_x1,x1],probs=[0.1, 0.9])
+p3 = opp_sum(name = "p3",inherit=[n_x2,x2],probs=[0.4, 0.6])
+network.append((p1, p2, p3))
+
+# The multiply operators
+m1 = opp_multi(name = "m1",inherit=[p1,p3])
+m2 = opp_multi(name = "m2",inherit=[p2,p3])
+network.append((m1, m2))
+
+# The final plus operator
+p4 = opp_sum(name = "p1",inherit=[m1,m2],probs=[0.3, 0.7])
+network.append([p4])
+
+spn.setStructure(network=network, variables = [n_x1, x1, n_x2, x2])
+
+variables = [0.0,1.0,1.0,1.0]
+
+print("The probability is: ", spn.eval(variables))
---
+++ SumProductNetwork/SumProductNetwork.py
@@ -0,0 +1,77 @@
+# Die processing kan dalk baie vinniger gemaak word deur om direk na die variables te point
+
+import numpy as np
+class SPN:
+    def __init__(self):
+        print("I am created!")
+
+    def setStructure(self, network, variables):
+        self.input_values = variables
+        self.network = network
+    # Each variable can be in one of its states or 'U' for unknown
+    def eval(self,values):
+        # Initialize the input values
+        for index in range(len(self.input_values)):
+            self.input_values[index].set_value(values[index])
+
+        # Run every node in the network
+        for net_depth in range(len(self.network)):
+            for node_index in range(len(self.network[net_depth])):
+                print(net_depth," ",node_index," ",self.network[net_depth][node_index].calc_value())
+        return self.network[len(self.network)-1][0].get_value()
+
+    # def fit(self, data):
+    #     pass
+    #
+    # def findStructure(self, data):
+    #     pass
+    #
+    # def loadNetwork(self, filename = ""):
+    #     pass
+    #
+    # def saveNetwork(self, filename = ""):
+    #     pass
+
+class variable():
+    def set_value(self,value):
+        self.value = value
+    def get_value(self):
+        return self.value
+
+class opp_sum():
+    def __init__(self,name="", inherit = [], probs = []):
+        if len(inherit) != len(probs):
+            print("Deminsions not the same for inherit",len(inherit)," and probs: ", len(probs))
+            return None
+        self.name = name
+        self.inherit = inherit
+        self.probs = probs
+
+    def calc_value(self):
+        self.total = 0.0
+        for index in range(len(self.inherit)):
+            self.total += self.inherit[index].get_value() * self.probs[index]
+        return self.total
+
+    def get_name(self):
+        return self.name
+
+    def get_value(self):
+        return self.total
+
+class opp_multi():
+    def __init__(self,name="", inherit = []):
+        self.name = name
+        self.inherit = list(inherit)
+
+    def calc_value(self):
+        self.total = 1.0
+        for index in range(len(self.inherit)):
+            self.total *= self.inherit[index].get_value()
+        return self.total
+
+    def get_name(self):
+        return self.name
+
+    def get_value(self):
+        return self.total
